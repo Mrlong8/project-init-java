5071a9364a41c8d75d21cd332348e9e0e511cf47
MrLong <xuanlongbui189@gmail.com>
2026-01-01 09:50:33 +0700

    innit project java

---
+++ src/hellloWord.java
@@ -0,0 +1,9 @@
+public class hellloWord {
+    static void main( String[] args) {
+        // psvm
+        // sout
+        System.out.println("xin Chào Bùi Xuan Long");
+        System.out.println("hello word");
+        System.out.println(123456);
+    }
+}
