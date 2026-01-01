3c8a0677f9df902be01cd15a6d918fce9d19e476
MrLong <xuanlongbui189@gmail.com>
2026-01-01 10:07:39 +0700

    update 1

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,10 @@
 public class HelloWorld {
     static void main(String[] args) {
         System.out.println("hello world Bui Xuan Long");
+        int a = 6;
+        int b = 9;
+        int c = a + b;
+        System.out.println("Sum : "+c);
+        
     }
 }
